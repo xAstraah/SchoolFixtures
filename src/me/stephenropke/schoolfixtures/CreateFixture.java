331150f480502618530ed0246b7ccb652b044f74
Stephen Ropke <stephen.ropke14@gmail.com>
2014-12-09 11:19:34 +0000

    Update CreateFixture.java

--- src/me/stephenropke/schoolfixtures/CreateFixture.java
+++ src/me/stephenropke/schoolfixtures/CreateFixture.java
@@ -7,13 +7,18 @@ package me.stephenropke.schoolfixtures;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  *
  * @author Stephen
  */
 public class CreateFixture extends Activity {
-
+	
+	Button submit;
+	
     /**
      * Called when the activity is first created.
      */
@@ -21,6 +26,16 @@ public class CreateFixture extends Activity {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.createfixture);
+        
+        submit = (Button) findViewById(R.id.submitfixture);
+        
+        submit.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), "Feature not yet implemented!", Toast.LENGTH_LONG).show();
+			}
+		});
     }
     
 }

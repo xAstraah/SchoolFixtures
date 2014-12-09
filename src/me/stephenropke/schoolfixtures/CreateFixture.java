fb36ed786e72cc8da580a0b62b120cea6e0d85fe
Stephen <Stephen@Stephen-PC>
2014-12-09 21:53:41 +0000

    Initial commit.

--- src/me/stephenropke/schoolfixtures/CreateFixture.java
+++ src/me/stephenropke/schoolfixtures/CreateFixture.java
@@ -7,13 +7,19 @@ package me.stephenropke.schoolfixtures;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  *
  * @author Stephen
  */
 public class CreateFixture extends Activity {
-
+    
+    EditText type, desc, date, result;
+    Button submit;
+    
     /**
      * Called when the activity is first created.
      */
@@ -21,6 +27,22 @@ public class CreateFixture extends Activity {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.createfixture);
+        
+        type = (EditText) findViewById(R.id.type);
+        desc = (EditText) findViewById(R.id.desc);
+        date = (EditText) findViewById(R.id.date);
+        result = (EditText) findViewById(R.id.result);
+        submit = (Button) findViewById(R.id.submit);
+        
+        submit.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                String type;
+                String desc;
+                String date;
+                String result;
+            }
+        });
     }
     
 }

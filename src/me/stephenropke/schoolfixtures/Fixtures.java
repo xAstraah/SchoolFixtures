54a1f822e17485c0ad4c496e3fa4f4bbcf28ed42
Stephen <Stephen@Stephen-PC>
2014-12-11 18:40:31 +0000

    Initial commit.

--- src/me/stephenropke/schoolfixtures/Fixtures.java
+++ src/me/stephenropke/schoolfixtures/Fixtures.java
@@ -8,9 +8,25 @@ package me.stephenropke.schoolfixtures;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  *
@@ -28,6 +44,11 @@ public class Fixtures extends Activity {
         super.onCreate(icicle);
         setContentView(R.layout.fixtures);
         
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        
+        connect();
+        
         addfixture = (Button) findViewById(R.id.addfixture);
         
         addfixture.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +64,41 @@ public class Fixtures extends Activity {
                 startActivity(new Intent(Fixtures.this, CreateFixture.class));
             }
         });
+    }
+    
+    public void connect() {
+        String data;
+        List<String> r = new ArrayList<String>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, r);
+        ListView list = (ListView) findViewById(R.id.listView1);
+        try {
+            DefaultHttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet("http://10.0.2.2/retrievedata.php");
+            HttpResponse response = client.execute(request);
+            HttpEntity entity=response.getEntity();
+            data=EntityUtils.toString(entity);
+            Log.e("STRING", data);
+            try {
+                JSONArray json = new JSONArray(data);
+                for(int i = 0; i < json.length(); i++) {
+                    JSONObject obj = json.getJSONObject(i);
+                    String type = obj.getString("type");
+                    String description = obj.getString("description");
+                    String date = obj.getString("date");
+                    String result = obj.getString("result");
+                    Log.e("STRING", type);
+                    String iteminlist = type + " : " + date;
+                    r.add(iteminlist);
+                    list.setAdapter(adapter);
+                }
+            } catch(JSONException e) {
+                e.printStackTrace();
+            }
+        } catch(ClientProtocolException e) {
+            Log.d("HTTPCLIENT", e.getLocalizedMessage());
+        } catch(IOException e) {
+            Log.d("HTTPCLIENT", e.getLocalizedMessage());
+        }
     }
     
 }

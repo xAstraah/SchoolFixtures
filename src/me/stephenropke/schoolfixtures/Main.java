8700e857c23dc5ce4836e0c7abbfba58a60f9442
Stephen Ropke <stephen.ropke14@gmail.com>
2014-12-12 13:34:15 +0000

    Update Main.java

--- src/me/stephenropke/schoolfixtures/Main.java
+++ src/me/stephenropke/schoolfixtures/Main.java
@@ -11,6 +11,10 @@ import android.widget.Toast;
 public class Main extends Activity
 {
     
+    /*
+    * Creates fields to be initialised later.
+    */
+    
     EditText username, password;
     Button login, skiplogin;
     
@@ -19,34 +23,76 @@ public class Main extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        
+        /*
+        * Tells the application to display the main.xml layout file on the screen.
+        */
+        
         setContentView(R.layout.main);
+        
+        /*
+        * Initialising the fields that we set earlier.
+        */
         
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
         login = (Button) findViewById(R.id.login);
         skiplogin = (Button) findViewById(R.id.skiplogin);
         
+        /*
+        * Tells the application what to do when the login button is clicked.
+        */
+        
         login.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
-                if(username.getText().toString().equals("1") && password.getText().toString().equals("1")) {
+                
+                /*
+                * Checks if the username and password equal what they should.
+                */
+                
+                if(username.getText().toString().equals("Mr.Ropke") && password.getText().toString().equals("Thenameisropke")) {
+                    
+                    /*
+                    * If the username and password they entered is correct it will give them access to add and update fixtures.
+                    */
+                    
                     Authentication a = new Authentication();
                     a.setUsername(username.getText().toString());
                     a.setAuthLevel(1);
                     Intent fixturesScreen = new Intent(Main.this, Fixtures.class);
                     startActivity(fixturesScreen);
                 } else {
+                    
+                    /*
+                    * If the username and password they entered is wrong it will show a popup saying "Incorrect username or password!"
+                    */
+                    
                     String incorrectunop = "Incorrect username or password!";
                     Toast.makeText(getApplicationContext(), incorrectunop, Toast.LENGTH_SHORT).show();
                 }
             }
         });
         
+        /*
+        * Tells the application what to do if the skiplogin button is clicked.
+        */
+        
         skiplogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                
+                /*
+                * This will set the user as a guest and they will only be able to view fixtures.
+                */
+                
                 Authentication a = new Authentication();
                 a.setUsername("Guest");
                 a.setAuthLevel(0);
+                
+                /*
+                * Will change the screen layout from main.xml to fixtures.xml.
+                */
+                
                 Intent fixturesScreen = new Intent(Main.this, Fixtures.class);
                 startActivity(fixturesScreen);
             }

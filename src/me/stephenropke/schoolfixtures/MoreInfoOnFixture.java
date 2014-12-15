8a6c55a86b3a748f148fadc9cfd99ac35759aca1
Stephen Ropke <stephen.ropke14@gmail.com>
2014-12-15 14:31:08 +0000

    Update MoreInfoOnFixture.java

--- src/me/stephenropke/schoolfixtures/MoreInfoOnFixture.java
+++ src/me/stephenropke/schoolfixtures/MoreInfoOnFixture.java
@@ -21,6 +21,15 @@ public class MoreInfoOnFixture extends Activity {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.moreinfoonfixture);
+        
+        /**
+         * TODO: Start/Finish the code for this class file.
+         * This class will display more information on a fixture when clicked.
+         * 
+         * Don't think this will get finished as i have now run out of time for programming because of the time frame for this
+         * assessment although we have been given extended time to complete which is practically Java Documentation.'
+         **/
+        
     }
     
 }
